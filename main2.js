da717971760672c8c8f8edecdceb8a0badbf8636
Freddy-M3363 <126246334+Freddy-M3363@users.noreply.github.com>
2023-10-09 23:38:37 +0300

    project added

---
+++ main2.js
@@ -0,0 +1,8 @@
+// javascript for toggle menu 
+var navBar = document.getElementById('nav-bar');
+function showMenu() {
+    navBar.style.right = '0';
+}
+function hideMenu(){
+    navBar.style.right = '-200px';
+}
